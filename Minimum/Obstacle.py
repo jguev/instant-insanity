c8f6dc53a5807f8d23e943fd341c68e541c79032
Johanna <johannaguevara17@gmail.com>
2021-12-12 23:48:51 -0800

    Added intro and adjusted the color generator to 31 slices

--- Minimum/Obstacle.py
+++ Minimum/Obstacle.py
@@ -1,12 +1,18 @@
+from rich import print as rprint
 from math import floor, pi, e
-from typing import List, Dict
+from typing import List
 
+# Known:
 # Triangular prism, 3 faces
-f1: List[int] = []
-f2: List[int] = []
-f3: List[int] = []
+# Colors are assigned counter-clockwise: f1 > f2 > f3
+# Six puzzles with predefined formulas
 
-# Puzzles required from assignment, 6
+# FACES
+f1: List[int] = []  # front
+f2: List[int] = []  # back on the right
+f3: List[int] = []  # back on the left
+
+# PUZZLES
 puzzleOne: List[List[int]] = []
 puzzleTwo: List[List[int]] = []
 puzzleThree: List[List[int]] = []
@@ -14,17 +20,43 @@ puzzleFour: List[List[int]] = []
 puzzleFive: List[List[int]] = []
 puzzleSix: List[List[int]] = []
 
-# Counting occurrences
-occurrencesCounter: Dict[int, List[List[int]]]
+########################   INTRO   ########################
 
-# Generate Data
+
+def intro():
+    rprint("[bold blue]Instant Insanity Puzzle[/bold blue]\n")
+    rprint("[blue]Generating colors with the given contraints ...[/blue]")
+
+    slices = len(puzzleOne)
+    rprint("[blue]Assigning counterclockwise to " + str(slices) +
+           " slices ...[/blue]\n")  # should be 31 slices
+    p = [puzzleOne, puzzleTwo, puzzleThree, puzzleFour, puzzleFive, puzzleSix]
+    f = [
+        "1 + ((floor n * 17 pi^6)mod 31)", "1 + ((floor n * 17 e^6)mod 31)", " 1 + ((floor n * 17 e^8)mod 31)",
+        "1 + ((floor n * 11 e^8)mod 31)", "1 + ((floor n * 101 e^2)mod 31)", "1 + ((floor n * 101 e^8)mod 31)"
+    ]
+    p_number = 0
+    for i in p:
+        p_number = p_number+1
+        rprint("\n[bold green]Puzzle " + str(p_number) +
+               ": " + f[p_number-1] + "[/bold green]")
+
+        # prints colors [ color of face 1, color of face 2, color of face 3]
+        print(p[p_number-1])
+
+
+# Colors
 # Puzzle 1: 1 + ((floor n * 17 pi^6)mod 31) ~ 1 + ((floor(n * (17 * pow(pi, 6)))) % 31)
 # Puzzle 2: 1 + ((floor n * 17 e^6)mod 31) ~ 1 + ((floor(n * (17 * pow(e, 6)))) % 31)
 # Puzzle 3: 1 + ((floor n * 17 e^8)mod 31) ~ 1 + ((floor(n * (17 * pow(e, 8)))) % 31)
 # Puzzle 4: 1 + ((floor n * 11 e^8)mod 31) ~ 1 + ((floor(n * (11 * pow(e, 8)))) % 31)
 # Puzzle 5: 1 + ((floor n * 101 e^2)mod 31) ~ 1 + ((floor(n * (101 * pow(e, 2)))) % 31)
 # Puzzle 6: 1 + ((floor n * 101 e^8)mod 31) ~ 1 + ((floor(n * (101 * pow(e, 8)))) % 31)
-for n in range(1, 93, 5):
+
+# Start: 1
+# End: 32 (essentially 31 iterations since we end at 1)
+# Step: 1 (default)
+for n in range(1, 32):
     puzzleOne.append([1 + ((floor(n * (17 * pow(pi, 6)))) % 31), 1 + ((floor((n+1)
                      * (17 * pow(pi, 6)))) % 31), 1 + ((floor((n+2) * (17 * pow(pi, 6)))) % 31)])
     puzzleTwo.append([1 + ((floor(n * (17 * pow(e, 6)))) % 31), 1 + ((floor((n+1)
@@ -39,10 +71,5 @@ for n in range(1, 93, 5):
                      * (101 * pow(e, 8)))) % 31), 1 + ((floor((n+2) * (101 * pow(e, 8)))) % 31)])
 
 
-# Fancy logic belongs here
-print(puzzleOne)
-print(puzzleTwo)
-print(puzzleThree)
-print(puzzleFour)
-print(puzzleFive)
-print(puzzleSix)
+if __name__ == '__main__':
+    intro()
